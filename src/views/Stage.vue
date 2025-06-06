6a955ba521aea5ea66b494707d65fd110776d680
lmerat-iut90 <146170893+lmerat-iut90@users.noreply.github.com>
2025-06-06 15:22:44 +0200

    Update Stage.vue

--- src/views/Stage.vue
+++ src/views/Stage.vue
@@ -15,7 +15,7 @@
           <div>
               <p>Talend est un logiciel de gestion de données qui utilise un processus ETL (Extract, Transform, Load). Ce processus permet de transformer des données brutes en informations exploitables pour la Business Intelligence.<br>
                   À mon arrivée dans l'entreprise, le traitement des données durait environ 2h30. Une fois ce traitement effectué, les données transformées sont récupérées par le logiciel Tableau, qui les utilise pour produire des visualisations sous forme de graphiques, de tableaux, et d'autres représentations visuelles.<br>
-              Mon but était de fiabiliser et d'optimiser les flux afin qu'il n'y ai le moins d'erreurs possibles lors de l'exécution des traitements, et de réduire le temps de l'exécution de ces derniers.</p>
+              Mon objectif était de fiabiliser et d’optimiser les flux afin de minimiser les erreurs lors de l’exécution des traitements, tout en réduisant leur temps d’exécution.</p>
           </div>
       </div>
   </div>
