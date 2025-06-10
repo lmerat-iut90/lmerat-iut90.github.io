0c9ba4f3a23183d497052901168983b0276e95e9
lmerat-iut90 <146170893+lmerat-iut90@users.noreply.github.com>
2025-06-10 11:23:39 +0200

    flux données

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -11,7 +11,11 @@ Un des problèmes majeurs identifiés concernait l’irrégularité des temps d�
 Cette instabilité avait un impact direct sur la qualité des restitutions dans Tableau : lorsque les flux prenaient trop de temps, le logiciel risquait de ne pas récupérer l’ensemble des données nécessaires à la génération des tableaux de bord, compromettant ainsi la fiabilité des analyses produites.
 
 Mon objectif principal était donc de fiabiliser et optimiser les flux Talend afin de réduire ces irrégularités, limiter les erreurs d’exécution, et améliorer globalement les performances, notamment en raccourcissant les durées de traitement.</p>
-
+    <div>
+      <img ref="@assets/organisation-flux.png" alt="flux">
+      <p><em>Figure 3 : Organisation des flux de données</em></p>
+    </div>
+    
 
   </div>
 </template>
