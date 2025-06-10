d150236142258414332c7580d8e215c4dbc8f1fc
lmerat-iut90 <146170893+lmerat-iut90@users.noreply.github.com>
2025-06-10 11:20:21 +0200

    Talend-Tableau

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -4,11 +4,13 @@
       <h2>Fiabilisation des flux Talend</h2>
       <p>Talend est un logiciel de gestion de données qui repose sur un processus ETL (Extract, Transform, Load). Ce processus permet de transformer des données brutes en informations exploitables pour la Business Intelligence.
 
-          À mon arrivée dans l'entreprise, le traitement complet des données durait environ 2h30. Une fois ce traitement terminé, les données transformées étaient récupérées par le logiciel Tableau, qui les utilisait pour produire des visualisations : graphiques, tableaux, et autres représentations visuelles.
+À mon arrivée dans l'entreprise, le traitement complet des données durait environ 2h30. Une fois ce traitement terminé, les données transformées étaient récupérées par le logiciel Tableau, qui les utilisait pour produire des visualisations : graphiques, tableaux, et autres représentations visuelles.
 
-          Un des problèmes majeurs identifiés concernait l’irrégularité des temps d’exécution : certains processus, normalement exécutés en 5 minutes, pouvaient exceptionnellement prendre jusqu’à 1 heure, sans raison apparente.
+Un des problèmes majeurs identifiés concernait l’irrégularité des temps d’exécution : certains processus, normalement exécutés en 5 minutes, pouvaient exceptionnellement prendre jusqu’à 1 heure, sans raison apparente.
 
-          Mon objectif principal était donc de fiabiliser et optimiser les flux Talend afin de réduire ces irrégularités, limiter les erreurs d’exécution, et améliorer globalement les performances, notamment en raccourcissant les durées de traitement.</p>
+Cette instabilité avait un impact direct sur la qualité des restitutions dans Tableau : lorsque les flux prenaient trop de temps, le logiciel risquait de ne pas récupérer l’ensemble des données nécessaires à la génération des tableaux de bord, compromettant ainsi la fiabilité des analyses produites.
+
+Mon objectif principal était donc de fiabiliser et optimiser les flux Talend afin de réduire ces irrégularités, limiter les erreurs d’exécution, et améliorer globalement les performances, notamment en raccourcissant les durées de traitement.</p>
 
 
   </div>
